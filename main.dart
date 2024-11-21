bb9ae5eb13ad3340a8864e16f839266541134bab
HarshadaDe <158428125+HarshadaDe@users.noreply.github.com>
2024-11-22 00:52:34 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(const ExpenseTrackerApp());
+}
+
+class ExpenseTrackerApp extends StatelessWidget {
+  const ExpenseTrackerApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Smart Expense Tracker',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const HomeScreen(),
+    );
+  }
+}
